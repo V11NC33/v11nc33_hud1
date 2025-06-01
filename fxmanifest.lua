50f8d89721ac067e141064c1f9d16b368bdd0ad1
V11NC33 <vincentgvcontact@gmail.com>
2025-06-01 19:22:44 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ui_page {
+	'html/index.html',
+}
+--discord.gg/avance
+--discord.gg/avance
+--discord.gg/avance
+--discord.gg/avance
+files {
+	'html/fonts/ttnorms/*.otf',
+	'html/fonts/ttnorms/*.ttf',
+	'html/fonts/*.otf',
+	'html/fonts/*.ttf',
+	'html/css/*.css',
+	'html/js/*.js',
+	'html/index.html',
+	"html/img/*.png",
+}
+--discord.gg/avance
+--discord.gg/avance
+--discord.gg/avance
+--discord.gg/avance
+client_scripts {
+	'client/*.lua',
+}
+--discord.gg/avance
+--discord.gg/avance
+--discord.gg/avance
+server_scripts {
+	'server/*.lua',
+}
+--discord.gg/avance
+--discord.gg/avance
